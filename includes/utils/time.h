8582bf70fcb2015422265669033266f56d927dee
noctuelles <plouvel@student.42.fr>
2024-02-06 04:42:05 +0100

    feat: added get_timeval_from_sec

--- includes/utils/time.h
+++ includes/utils/time.h
@@ -6,7 +6,7 @@
 /*   By: plouvel <plouvel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 06:20:52 by plouvel           #+#    #+#             */
-/*   Updated: 2024/02/01 06:21:28 by plouvel          ###   ########.fr       */
+/*   Updated: 2024/02/06 04:37:30 by plouvel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 #include <netinet/ip_icmp.h>
 
-double compute_round_trip_time(struct icmp *icmp_packet);
+double         compute_round_trip_time(struct icmp *icmp_packet);
+struct timeval get_timeval_from_sec(double seconds);
 
 #endif /* UTILS_TIME_H */
